d8242e5c1020649b3eed104a35ba0aa354da2b9b
flores-97 <floresvictor15@gmail.com>
2025-04-30 18:47:28 -0700

    currently working

---
+++ COMP003B.Assignment6ATTMPT2/Migrations/20250501014312_NEWAge.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COMP003B.Assignment6ATTMPT2.Migrations
+{
+    /// <inheritdoc />
+    public partial class NEWAge : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Actors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Actors");
+        }
+    }
+}
